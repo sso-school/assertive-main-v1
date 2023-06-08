93d2190e6f9437df1f2e940494628bdff9fbf640
최재민 <admin@chicken-moo.com>
2023-06-08 17:23:00 +0900

    Dockerfile Add & components folder change

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,8 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import React from "react";
 
-import DefaultHead from "./components/DefaultHead";
-
+import DefaultHead from "@/components/DefaultHead";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
