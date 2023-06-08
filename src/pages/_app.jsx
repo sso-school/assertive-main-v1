23fb611cd1c961dd87d9f2ec8803d8db5df5e6d5
최재민 <admin@chicken-moo.com>
2023-06-08 10:28:17 +0900

    first commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,10 @@
+import "@/styles/globals.css";
+import React from "react";
+import { RecoilRoot } from "recoil";
+export default function App({ Component, pageProps }) {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+
+import DefaultHead from "./components/DefaultHead";
+
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <DefaultHead></DefaultHead>
+      <main className={styles.main}>
+        
+      </main>
+    </>
+  );
+}
